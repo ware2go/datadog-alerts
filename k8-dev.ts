8cbfc00021a5708569ddbf11615140ee019ede7a
Michael D Roach <michael.r@ware2go.co>
2020-04-16 10:44:43 -0400

    On branch k8-dev-monitors  Your branch is up to date with 'origin/k8-dev-monitors'.
     Changes to be committed:
    	modified:   k8-dev.ts

--- k8-dev.ts
+++ k8-dev.ts
@@ -21,6 +21,29 @@ function messageFreeDiskSpace(notify: string): string {
   return `{{#is_alert}}\n{{cluster-name.name}} has less than 10GB of free disk space\n{{/is_alert}} \n{{#is_warning}}\n{{cluster-name.name}} has less than 20GB of free disk space\n{{/is_warning}}\n{{#is_recovery}}\n{{cluster-name.name}} disk space recovered.\n{{/is_recovery}}   ${notify}`;
 }
 
+// Cluster 5 minute load Average
+function queryFiveMinuteLoadAverages(environment: string): string {
+  return `avg(last_5m):avg:system.load.5{cluster-name:${environment}-primary by {cluster-name} > 7`;
+}
+function messageFiveMinuteLoadAverages(notify: string): string {
+  return `{{#is_alert}}\n{{cluster-name.name}} has 5 minute load average 7\n{{/is_alert}} \n{{#is_warning}}\n{{cluster-name.name}} has 5 minute load average 5\n{{/is_warning}}\n{{#is_recovery}}\n{{cluster-name.name}} recovered.\n{{/is_recovery}} ${notify}`;
+}
+
+// Cluster CPU IOWait
+function queryCpuIoWait(environment: string): string {
+  return `avg(last_5m):avg:system.cpu.iowait{cluster-name:${environment}-primary} by {cluster-name} > 0.5`;
+}
+function messageCpuIoWait(notify: string): string {
+  return `{{#is_alert}}\n{{cluster-name.name}}  System Cpu iowait greater than 0.5\n{{/is_alert}} \n{{#is_warning}}\n{{cluster-name.name}} System Cpu iowait greater than 0.3\n{{/is_warning}}\n{{#is_recovery}}\n{{cluster-name.name}} recovered.\n{{/is_recovery}} ${notify}`;
+}
+
+// Cluster Kubernetes Pods Expired
+function queryExpiredPods(environment: string): string {
+  return `avg(last_5m):avg:kubernetes.pods.expired{cluster-name:${environment}-primary} by {cluster-name} > 7`;
+}
+function messageExpiredPods(notify: string): string {
+  return `{{#is_alert}}\n{{cluster-name.name}}  Kubernetes Expired Pods 7\n{{/is_alert}} \n{{#is_warning}}\n{{cluster-name.name}} Kubernetes Expired Pods 5\n{{/is_warning}}\n{{#is_recovery}}\n{{cluster-name.name}} recovered.\n{{/is_recovery}} ${notify}`;
+}
 
 // Monitors
 export const devClusterMonitor = new datadog.Monitor(`${projectEnv}NoDataMonitor`, {
@@ -38,13 +61,55 @@ export const devClusterMonitor = new datadog.Monitor(`${projectEnv}NoDataMonitor
   thresholds: { critical: 75, warning: 50 },
 });
 
-export const devClusterCpuMonitor = new datadog.Monitor(`${projectEnv}NoDataMonitor`, {
+export const devClusterCpuMonitor = new datadog.Monitor(`${projectEnv}ClusterCpuMonitor`, {
   name: '{{cluster-name.name}} free disk space',
   type: 'metric alert',
   query: queryFreeDiskSpace(projectEnv),
   message: messageFreeDiskSpace(`${groupNotify}`),
   tags: [projectEnv],
   thresholds: { critical: 10000000000, warning: 20000000000 },
+  notifyNoData: false,
+  newHostDelay: 300,
+  includeTags: true,
+  requireFullWindow: true,
+  timeoutH: 0,
+});
+
+export const devClusterFiveMinuteLoadAverage = new datadog.Monitor(`${projectEnv}FiveMinuteLoadAverage`, {
+  name: '{{cluster-name.name}} Five Minute Load Average',
+  type: 'metric alert',
+  query: queryFiveMinuteLoadAverages(projectEnv),
+  message: messageFiveMinuteLoadAverages(`${groupNotify}`),
+  tags: [projectEnv],
+  thresholds: { critical: 7, warning: 5 },
+  notifyNoData: false,
+  newHostDelay: 300,
+  includeTags: true,
+  requireFullWindow: true,
+  timeoutH: 0,
+});
+
+export const devClusterCpuIoWait = new datadog.Monitor(`${projectEnv}CpuIoWait`, {
+  name: '{{cluster-name.name}} Five Minute Load Average',
+  type: 'metric alert',
+  query: queryCpuIoWait(projectEnv),
+  message: messageCpuIoWait(`${groupNotify}`),
+  tags: [projectEnv],
+  thresholds: { critical: 0.5, warning: 0.3 },
+  notifyNoData: false,
+  newHostDelay: 300,
+  includeTags: true,
+  requireFullWindow: true,
+  timeoutH: 0,
+});
+
+export const devClusterExpiredPods = new datadog.Monitor(`${projectEnv}ExpiredPods`, {
+  name: '{{cluster-name.name}} Five Minute Load Average',
+  type: 'metric alert',
+  query: queryExpiredPods(projectEnv),
+  message: messageExpiredPods(`${groupNotify}`),
+  tags: [projectEnv],
+  thresholds: { critical: 7, warning: 5 },
   notifyNoData: false,
   newHostDelay: 300,
   includeTags: true,
